f7ac7b2ea5bea5667b4d1522533d0aa0dec213ea
Jonathan Paiva <jonathan.paiva.m@hotmail.com>
2021-07-26 18:46:52 -0300

    Adicionado informações referente ao uso de definição de propriedades com o Data Annotations, usando diversos tipos.

--- Estudos/Entidades.cs
+++ Estudos/Entidades.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Estudos
 {
@@ -13,6 +14,7 @@ namespace Estudos
             //Configurações sobre o DataAnnotations
             [Required] // Propriedades 
             [StringLength(200)]  // Propriedades 
+            [Column("NomeProduto")] // Propriedades - Nome da Coluna
             public string NomeItem { get; set; }
             //-------------------------------------
 
@@ -32,7 +34,13 @@ namespace Estudos
 
         public class Aluno
         {
+            [Key] //Indica a Chave primária da tabela ignorando a convensão
+            [DatabaseGenerated(DatabaseGeneratedOption.None)] //.Computed = Valor gerado ao salvar e regenerado ao ser atualizado / .Identity = Gerado pelo Banco de Dados na Adição / .None = não gera auto numeração
+            public int Codigo { get; set; }
+            [NotMapped] // Atributo não mapeia a informação da coluna e não cria ela no banco de dados. 
             public int AlunoID { get; set; }
+            [Required] // Atributo exige o preenchimento da informação - not null
+            [MaxLength(150)] //Define o tamanho máximo do campo
             public string Nome { get; set; }
             public Equipamento Equipamento { get; set; } //Propriedade de navegação - Relacionamento um-para-um
         }
@@ -43,6 +51,22 @@ namespace Estudos
             public string Nome { get; set; }
             public Aluno Aluno{ get; set; } //Propriedade de navegação - Relacionamento um-para-um
             public int AlunoID { get; set; } //Chave Estrangeira
+        }
+
+        public class Autor
+        {
+            public int AutorID { get; set; }
+            public string Nome { get; set; }
+            [ForeignKey("AutorFK")] // Pode ser definido de várias formas, neste caso, apenas utilizar a propriedade 'ForeignKey' Indicando qual é o nome da coluna da Entidade que está relacionada para vincular a chave estrangeira
+            public ICollection<Livro> Livros { get; set; }
+        }
+
+        public class Livro
+        {
+            public int LivroID { get; set; }
+            public string Nome { get; set; }
+            public Autor Autor { get; set; }
+            public int AutorFK { get; set; }
         }
     }
 }
